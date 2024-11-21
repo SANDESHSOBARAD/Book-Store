cf09433385f9f3a09f2a917c8b7e8eb4dc043d22
SANDESHSOBARAD <sobaradsandesh@gmail.com>
2024-11-21 15:57:24 -0800

    Home and ShowBook components ready

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -49,6 +49,9 @@ const Home = () => {
           <th className='border border-slate-600 rounded-md max-md:hidden'>
             Publish Year
           </th>
+          <th className='border border-slate-600 rounded-md max-md:hidden'>
+            Price
+          </th>
           <th className='border border-slate-600 rounded-md'>Operations</th>
         </tr>
       </thead>
@@ -66,6 +69,9 @@ const Home = () => {
             </td>
             <td className='border border-slate-700 rounded-md text-center max-md:hidden'>
               {book.publishYear}
+            </td>
+            <td className='border border-slate-700 rounded-md text-center max-md:hidden'>
+              {book.price}
             </td>
             <td className='border border-slate-700 rounded-md text-center'>
               <div className='flex justify-center gap-x-4'>
